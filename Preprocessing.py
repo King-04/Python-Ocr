f0a5b133d213bec2a6da8ac298fbab368171cfc4
Kenneth Boateng <86114969+King-04@users.noreply.github.com>
2021-12-02 02:51:30 -0800

    Preprocess

---
+++ Preprocessing.py
@@ -0,0 +1,15 @@
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#read image
+img = cv2.imread('inv1.jpg') 
+
+#preprocessing
+
+#grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale
+
+#black and white
+(thresh, bw) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
